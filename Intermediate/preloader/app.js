1bc0a2ea6a1def4077fdf1940ecc8bf0cd9a8474
Razvan Bogdan <razvanbogdanturiatca@gmail.com>
2021-11-06 16:44:42 +0200

    update code

--- Intermediate/preloader/app.js
+++ Intermediate/preloader/app.js
@@ -1,11 +1,11 @@
-let pp1 = new ProgressPreloader(window["prel-1"], 200, 10)
+// let pp1 = new ProgressPreloader(window["prel-1"], 200, 10)
 
 
-console.log(pp1.getTimerId())
+// console.log(pp1.getTimerId())
 
-// console.log(pp1)
+// // console.log(pp1)
 
-let pp2 = new CircularPreloader(window["prel-2"], 500, 250)
+// let pp2 = new CircularPreloader(window["prel-2"], 500, 250)
 
 // let pp3 = new IncompletePreloader(window["prel-3"], 200, 10)
 

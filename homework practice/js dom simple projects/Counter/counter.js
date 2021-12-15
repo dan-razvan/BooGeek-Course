7caba5cc8ba5b1eae9f285cdd7f6f5b11b9ed0b4
Razvan Bogdan <razvanbogdanturiatca@gmail.com>
2021-12-15 14:09:39 +0200

    update counter.js

--- homework practice/js dom simple projects/Counter/counter.js
+++ homework practice/js dom simple projects/Counter/counter.js
@@ -1,21 +1,20 @@
 const button1 = document.getElementById("first-button")
 const button2 = document.getElementById("second-button")
 const counter = document.getElementById("counter")
-let counterValue = counter.innerHTML;
-console.log(counterValue+1)
+
 
 let temp = 1;
 
 
 button1.addEventListener("click", ()=> {
     temp++
-    counterValue = temp;
+    counter.innerHTML  = temp;
     
 })
 
 button2.addEventListener("click", () => {
     if(temp > 1){
         temp--;
-        counterValue = temp;
+        counter.innerHTML = temp;
     }
 })

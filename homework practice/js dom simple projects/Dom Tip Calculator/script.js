b2d4ba87bb47006d999f8b5c99209735f6ba1f65
Razvan Bogdan <razvanbogdanturiatca@gmail.com>
2021-12-21 01:02:38 +0200

    update tip calculator

--- homework practice/js dom simple projects/Dom Tip Calculator/script.js
+++ homework practice/js dom simple projects/Dom Tip Calculator/script.js
@@ -6,8 +6,7 @@ const tipDisplay = document.querySelector(".display-tip")
 
 
 calculate.addEventListener("click", () => {
-    tip = calculateTip(billAmount, numOfPersons())
-    console.log(calculateTip(billAmount, numOfPersons()))
+    tip = calculateTip(billAmount, currentServiceLevel(), numOfPersons())
     tipDisplay.innerText = tip;
 })
 
@@ -19,9 +18,7 @@ bill.addEventListener("input", ()=>{
 
 let numOfPersons = () => {
     for(let i = 0; i < options.children.length-1; i++) {
-        console.log(options.children[i])
         if(options.children[i].selected){
-            console.log(i)
             return i+1
         }
     }
@@ -30,28 +27,32 @@ let numOfPersons = () => {
 let currentServiceLevel = () => {
     for(let i = 0; i<serviceLevels.length; i++){
         if(serviceLevels[i].checked){
-            return serviceLevels[i].id
+            return serviceLevels[i].value
         }
     }
 }
 
-const calculateTip = (bill,  persons) =>{
-    switch(currentServiceLevel()){
-        case "outstanding":
-            return `$${((bill  * 0.3) / persons).toFixed(2)}`
-            break;
-        case "good":
-            return `$${(bill  * 0.2 / persons).toFixed(2)}`
-            break;
-        case "okish":
-            return `$${(bill  * 0.15 / persons).toFixed(2)}`
-            break;
-        case "bad":
-            return `$${(bill  * 0.1 / persons).toFixed(2)}`
-            break;
-        case "never":
-            return `$${(bill  * 0 / persons).toFixed(2)}`
-            break;
-    }
+const calculateTip = (bill, value, persons) => {
+    return `$${(bill * value / persons).toFixed(2)}`
 }
 
+
+// const calculateTip = (bill,  persons) =>{
+//     switch(currentServiceLevel()){
+//         case "outstanding":
+//             return `$${((bill  * 0.3) / persons).toFixed(2)}`
+//             break;
+//         case "good":
+//             return `$${(bill  * 0.2 / persons).toFixed(2)}`
+//             break;
+//         case "okish":
+//             return `$${(bill  * 0.15 / persons).toFixed(2)}`
+//             break;
+//         case "bad":
+//             return `$${(bill  * 0.1 / persons).toFixed(2)}`
+//             break;
+//         case "never":
+//             return `$${(bill  * 0 / persons).toFixed(2)}`
+//             break;
+//     }
+// }

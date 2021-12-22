290c77eaabc2bf8985b79bd0b4fd4eee431a02cd
Razvan Bogdan <razvanbogdanturiatca@gmail.com>
2021-12-22 23:43:41 +0200

    update dom tip calculator

--- homework practice/js dom simple projects/Dom Tip Calculator/script.js
+++ homework practice/js dom simple projects/Dom Tip Calculator/script.js
@@ -6,7 +6,7 @@ const tipDisplay = document.querySelector(".display-tip")
 
 
 calculate.addEventListener("click", () => {
-    tip = calculateTip(billAmount, currentServiceLevel(), numOfPersons())
+    tip = calculateTip(billAmount, currentServiceLevel(), options.value)
     tipDisplay.innerText = tip;
 })
 
@@ -16,13 +16,6 @@ bill.addEventListener("input", ()=>{
     
 })
 
-let numOfPersons = () => {
-    for(let i = 0; i < options.children.length-1; i++) {
-        if(options.children[i].selected){
-            return i+1
-        }
-    }
-}
 
 let currentServiceLevel = () => {
     for(let i = 0; i<serviceLevels.length; i++){
@@ -36,23 +29,3 @@ const calculateTip = (bill, value, persons) => {
     return `$${(bill * value / persons).toFixed(2)}`
 }
 
-
-// const calculateTip = (bill,  persons) =>{
-//     switch(currentServiceLevel()){
-//         case "outstanding":
-//             return `$${((bill  * 0.3) / persons).toFixed(2)}`
-//             break;
-//         case "good":
-//             return `$${(bill  * 0.2 / persons).toFixed(2)}`
-//             break;
-//         case "okish":
-//             return `$${(bill  * 0.15 / persons).toFixed(2)}`
-//             break;
-//         case "bad":
-//             return `$${(bill  * 0.1 / persons).toFixed(2)}`
-//             break;
-//         case "never":
-//             return `$${(bill  * 0 / persons).toFixed(2)}`
-//             break;
-//     }
-// }
